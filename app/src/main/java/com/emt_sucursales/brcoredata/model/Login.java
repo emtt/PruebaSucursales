69ee090262ca9af7792f444b604689266a343548
emtt <emoralest@gmail.com>
2018-09-08 13:26:02 -0500

    Login process

---
+++ app/src/main/java/com/emt_sucursales/brcoredata/model/Login.java
@@ -0,0 +1,31 @@
+package com.emt_sucursales.brcoredata.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Login {
+
+    @SerializedName("user")
+    @Expose
+    private String user;
+    @SerializedName("password")
+    @Expose
+    private String password;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
---
+++ app/src/main/java/com/emt_sucursales/brcoredata/rest/ProjectRepository.java
@@ -0,0 +1,78 @@
+package com.emt_sucursales.brcoredata.rest;
+
+import android.arch.lifecycle.MutableLiveData;
+
+import com.emt_sucursales.brcoredata.Constants;
+import com.emt_sucursales.brcoredata.model.Login;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ProjectRepository {
+    private static String TAG = ProjectRepository.class.getSimpleName();
+    private static ProjectRepository projectRepository;
+    private APIInterface apiInterface;
+
+    public synchronized static ProjectRepository getInstance() {
+        if (projectRepository == null) {
+            if (projectRepository == null) {
+                projectRepository = new ProjectRepository();
+            }
+        }
+        return projectRepository;
+    }
+
+    private ProjectRepository() {
+
+        /*
+        Comment by Efrain Morales
+        We can create an interceptor for check the traffic using HttpLoggingInterceptor
+        or ChuckInterceptor which isn't added to this project. So I leave this comment just for info.
+
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        or
+
+        httpClient.addInterceptor(new ChuckInterceptor(App.getAppContext()));  // <-- this is the important line!
+        */
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Constants.SERVER)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient.build())
+                .build();
+
+        apiInterface = retrofit.create(APIInterface.class);
+
+    }
+
+
+    public MutableLiveData<Login> login(Login mLogin) {
+        final MutableLiveData<Login> data = new MutableLiveData<>();
+        apiInterface.login(mLogin).enqueue(new Callback<Login>() {
+            @Override
+            public void onResponse(Call<Login> call, Response<Login> response) {
+                if (response.isSuccessful()) {
+                    data.postValue(response.body());
+                } else {
+                    data.postValue(null);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Login> call, Throwable t) {
+                data.postValue(null);
+            }
+        });
+
+        return data;
+
+    }
+}
