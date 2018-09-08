c8a4749c54e6e35dc3ef219433514bdfb6b0387e
emtt <emoralest@gmail.com>
2018-09-08 12:53:14 -0500

    Update structure

--- app/src/main/java/com/emt_sucursales/views/MainActivity.java
+++ app/src/main/java/com/emt_sucursales/views/MainActivity.java
@@ -1,7 +1,9 @@
-package com.emt_sucursales;
+package com.emt_sucursales.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.emt_sucursales.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/emt_sucursales/views/MapsActivity.java
+++ app/src/main/java/com/emt_sucursales/views/MapsActivity.java
@@ -1,8 +1,9 @@
-package com.emt_sucursales;
+package com.emt_sucursales.views;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.emt_sucursales.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
